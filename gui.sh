2b9c5643027e42e15fb141a22e5c900847411e3c
Asem Mohamed Ali <167926594+Asem-Mohamed-321@users.noreply.github.com>
2024-12-20 20:53:32 +0200

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -11,24 +11,12 @@ do
 	flag2=true
 	choice=$(zenity --list --height=500 --title="Our DBMS" --column="CHOOSE AN OPERATION>" "1)create database" "2)list database" "3)connect to database" "4)drop database" "exit")
 
-	#echo "--------------------------------------"
-        #echo "1)create database "
-        #echo "2)list database "
-        #echo "3)connect to database "
-        #echo "4)drop database "
-        #echo "5)exit"
-	#echo "--------------------------------------"
-        #read -p "please enter your choice: " choice
         case $choice in
                 "1)create database")
 			db_name=$(zenity --entry --title="Creating a new database" --text="enter the name of the database:")
-			#read -p "enter the name of the database: " db_name
-
                         mkdir /home/$USER/bash-project/$db_name
                         mkdir /home/$USER/bash-project/$db_name/constraints_files
 			zenity --info --title="Creating a new database" --text="the db has been created successfully"
-			#echo "the db has been created"
-                        #ClicktoClear
                         ;;
                 "2)list database") echo "choosed $choice" #lists all the dbs
                         if [ "$(ls -A)" ]
@@ -36,13 +24,10 @@ do
                                 zenity --info --title="listing the databases" --text="`ls /home/$USER/bash-project`"
                         else
                                 zenity --info --title="listing the databases" --text="There are no Databases found"
-				#echo "There are no DataBase found"
                         fi
-                        #ClicktoClear
                         ;;
                 "3)connect to database") echo "choosed $choice"       #connect to database
                         db_name=$(zenity --entry --title="Connect to a database" --text="enter the database name:")
-			#read -p "Enter DataBase name: " db_name
                         if [[  -e "/home/$USER/bash-project/$db_name" ]]
                         then
                                 cd /home/$USER/bash-project/$db_name
@@ -51,63 +36,32 @@ do
                                 do
 					db_action=$(zenity --list --height=500 --title="$db_name database" --column="CHOOSE AN OPERATION" "1) Create Table" "2) List Tables" "3) Drop Table" "4) Insert into Table" "5) Select from Table" "6) Delee from Table" "7) Update Table" "8) back" "9) exit")
 
-                                	#echo "----------$db_name database-----------"
-                                        #echo "1) Create Table"
-                                        #echo "2) List Tables"
-                                        #echo "3) Drop Table"
-                                        #echo "4) Insert into Table"
-                                        #echo "5) Select from Table"
-                                        #echo "6) Delee from Table"
-                                        #echo "7) Update Table"
-                                        #echo "8) back"
-                                        #echo "9) exit"
-					#echo "--------------------------------------"
-                                        #read -p "Please Enter your choice: " db_action
                                         case $db_action in	#create table
                                                 "1) Create Table") echo "$db_action" #Create table
 							table_name=$(zenity --entry --title="Create table" --text="enter the name of the table :")
-							#read -p "Enetr the name of the table: " table_name
                                                         if [[ -e $table_name ]]
                                                         then
                                                                 zenity --info --title="Create table" --text="$table_name Already Exists"
-								#echo "$table_name Already Exists"
                                                         else
                                                                 touch $table_name
-								#echo "Enter the fields name (e.g(name1 name2 name3)) "
-								#read  -a attributes
-                                                		#split($attributes, fsplit, ",")
-								#echo $fsplit[@] > $table_name
-								#echo $(IFS=:; echo "${attributes[*]}") >> $table_name
-								#constraints
 								declare -a const	#constraints
 								declare -a att		#attributes
 								declare -a pk_arr       #Primary key 
                                                                 unset pk_arr
 								unset const
 								unset att
-								#const[0]=$table_name
-								#esc="y"
-								#typeset -i i=0
 								i=0
 								while true
 								do
 									att[$i]=$(zenity --entry --title="Create table" --text="enter the attribute name:")
 
-									#echo "Enter the attribute name: "
-									#read att[$i]
 									const[$i]=$(zenity --entry --title="Create table" --text="enter the corresponding constraint:")
-									#echo "Enter the corresponding constraint:"
-									#read const[$i]
 									if zenity --question --title="Create table" --text="Do you want to add more fields?"
 									then
 										((i++))
 									else
 										break
 									fi
-									#echo "do you wan to add more fields(y/n): "
-									#read esc
-									#((i++))
-
 								done
 								pk_arr="PK"
 								pk_name=$(zenity --entry --title="Create table" --text="Enter the Primary key attribute name: ")
@@ -128,7 +82,6 @@ do
 
                                                                 zenity --info --text="table created successfully !!"
                                                         fi
-                                                        #ClicktoClear
                                                 ;;
                                         "2) List Tables") echo "$db_action" #list table
                                                 if [ "$( ls -p | egrep -v /$ )" ]
@@ -136,13 +89,10 @@ do
                                                        	zenity --info --title="List Tables" --text="`ls -p | egrep -v /$`"
                                                 else
                                                         zenity --info --title="List Tables" --text="There are no tables in the database"
-							#echo "There are no tables in this DataBase"
                                                 fi
-                                                #ClicktoClear
                                                 ;;
                                         "3) Drop Table") echo "$db_action" #drop table
                                                 table_name=$(zenity --entry --title="Drop Table" --text="enter the name of the table :")        
-						#read -p "Enter the name of the table: " table_name
                                                         if [[ -e $table_name ]]
                                                         then
                                                                 rm $table_name
@@ -150,13 +100,10 @@ do
                                                                 zenity --info --title="Drop Table" --text="$table_name has been deleted"
                                                         else
                                                                 zenity --info --title="Drop Table" --text="$table_name not found"
-								#echo "$table_name not found"
                                                         fi
-                                                        #ClicktoClear
                                                         ;;
                                         "4) Insert into Table") echo "$db_action"     #Insert
                                                 table_name=$(zenity --entry --title="Insert Into Table" --text="enter the name of the table :")
-						#read -p "Enter the  Table name : " table_name
                                                 if [[ -e $table_name ]]
                                                 then
 							declare -a array
@@ -172,26 +119,14 @@ do
 							IFS=':' read -ra pk_arr <<< ${pk_tmp[@]}
 
                                                 	IFS=':' read -ra all_fields < $table_name 	#read attributes from file
-                                                	#temp=`tail -n 1 constraints_files/$table_name`
                                                 	IFS=':' read -ra const < constraints_files/$table_name 	#read attributes from file
 							#IFS=':' read -ra const < $temp #read attributes from file
-							#echo ${const[@]}
-							#read -p "enter the ${all_fields[0]} )" array[0]
-							#set -x
+
                            	               		for ((i=0;i<${#all_fields[@]};i++))
                                 	                do
 								element=$(zenity --entry --title="Insert Into Table" --text="enter the  ${all_fields[i]} :")
-								#read -p "enter the  ${all_fields[i]} )" element
 								if [[ ${const[$i]} == "int" ]]
 								then
-									#case $element in 
-									#	*([[:alnum:]]) ) echo "Number"
-									#		;;
-									#		
-									#	*) echo "wrong input data type"
-									#		break
-									#		;;
-									#esac
 									if [[ $element =~ ^[0-9]+$ ]]
 									then
 										echo "Number"
@@ -217,16 +152,6 @@ do
 										zenity --error --text="wrong input data type"
                                                                                 break 
 									fi
- 
-
-									#case $element in 
-                                                                        #*([[:alpha:]]) ) echo "alpha"
-									#	;;
-									#
-									#*) echo "wrong input data type"
-									#	break
-									#	;;
-									#esac
 								fi
 								if [[ ${all_fields[i]} == ${pk_arr[1]} ]]
                                                                 then
@@ -247,17 +172,9 @@ do
                                                                 else
                                                                         array[$i]=$element
                                                                 fi
-
-								#array=("${array[@]}"":$element")
-        	                                        
-								#array[$i]=$element
-							done
-
-							#set +x
-							
+							done							
 							if [[ ${#array[@]} -eq ${#all_fields[@]} ]]
 							then
-
                 	                                	array_a=$(IFS=:; echo "${array[*]}")
 								echo "${array_a[@]}" >> $table_name
 							else
@@ -266,22 +183,17 @@ do
 						else
 							zenity --info --title="Insert Into Table" --text="Table doesn't exist (create table first)"
 						fi
-                        	                #ClicktoClear
                                 	                ;;
 
                                         "5) Select from Table") echo "$db_action" 	#Select
 						table_name=$(zenity --entry --title="Select From Table" --text="Enter the table name :")
-                                                #read -p "Enter the  Table name : " table_name
-                                                #declare -a desired_fields
 						if [[ -e $table_name ]]
 						then
 
 						unset desired_fields
 						set -f #this line disable "globing" which will allow the use to input the character "*" without converting it 
 						desired_fields=$(zenity --entry --title="Insert Into Table" --text="enter the fields : \nthe format should be (name1 name2 name3)")
-						#echo "Enter the fields: (e.g (name1 name2 name3) )"
 						IFS=" " read -ra desired_fields <<< "$desired_fields"
-						#read -a desired_fields
 
 
 						
