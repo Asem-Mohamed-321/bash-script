8ed4611ddb01966f0cd12b521dacfaa6d246d626
Asem Mohamed Ali <167926594+Asem-Mohamed-321@users.noreply.github.com>
2024-12-20 21:03:25 +0200

    gui.sh last

--- gui.sh
+++ gui.sh
@@ -220,9 +220,7 @@ do
                                                         	do
                                                                 	if [[ ${desired_fields[i]} == ${all_fields[j]}  ]]
                                                                 	then
-                                                                        	#echo $j #debugging
                                                                         	last_fields=("${last_fields[@]}" "$(($j+1))")
-                                                                        	#echo ${last_fields[@]} #debugging
                                                                         	found=true
                                                                 	fi
                                                         	done
@@ -230,30 +228,16 @@ do
                                                                 	then
                                                                         	zenity --error --text="${desired_fields[i]} not found "
                                                                         	break
-		
                                                             		fi
                                                 	done
-
-						
 						fi
-
 						set +f #re-enabling globing
-
-                                                #desired_index=$(IFS=,; echo "${desired_fields[*]}") #serialization into one string to pass it to the awk then we will split it inside of the awk
-
 						if [[ $found == true ]]
 						then
-
-							#desired_fields=(1 3 5)
-                                                	#echo "Enter the field numbers to print (e.g., 1 3 5):"
-                                                	#read -a desired_fields
 							declare condition_arr
 							unset condition_arr
 							condition=""
 							condition_arr=$(zenity --entry --title="Select From Table" --text="Enter the condition : \nthe format shoulc be (e.g., salary > 5000) ")
-
-							#echo "Enter the condition (e.g., salary > 5000):"
-                                                        #read -a condition_arr
 							IFS=" " read -ra condition_arr <<< "$condition_arr"
 							condition_flag=false
 							if [[ ${#condition_arr[@]} -ne 0  ]]
@@ -280,62 +264,30 @@ do
 								fi
 
 							else
-							#	zenity --error --text="condition_arr is empty"
-                                                        	#condition_flag=false
-								condition=0
-									
+								condition=0		
                                                		fi
 
-							#for ((j=0;j<${#all_fields[@]};j++))
-                                                        #do
-								#if $condition_arr
-                                                                #then
-                                                                        #if [[ ${condition_arr[0]} == ${all_fields[$j]} ]]
-									#then
-										#condition=$j
-									#fi
-								#fi
-                                                        #done
-
-
-							
 							index=$(IFS=,; echo "${last_fields[*]}") #serialization into one string to pass it to the awk then we will split it inside of the awk
                                                 	desired_fields_s=$(IFS=,; echo "${desired_fields[*]}")
-                                                	#echo $desired_fields_s #for debugging
                                                 	f_count=${#desired_fields[@]}
-                                                	#set -x
 							condition_f="\$$condition ${condition_arr[1]} ${condition_arr[2]}"
 							echo $condition_f
 							zenity --info   --title="Select From Table" --width=300 --text="<tt>`awk -F: -v fields="$index" -v headline="$desired_fields_s" -v total_length=15 -v fields_count="$f_count" 'BEGIN {printf "%s", "+";for(j=0;j<fields_count;j++){for(i=0;i<15;i++) {printf "%s","-"};printf"+"}; print""; printf "|" ;split(headline, h, ",") ;for (i in h) {padding=total_length-length(h[i]);right=int(padding/2);left=padding-right; printf "%*s%s%*s|",left, "", h[i],right,"" };print""; printf "%s", "+"; for(j=0;j<fields_count;j++){for(i=0;i<15;i++) {printf "%s","-"};printf"+"}; print""} { if ( NR != 1 && '"$condition_f"' ) { printf "%s", "|"; split(fields, f, ",") ; for (i in f) {padding=total_length-length($f[i]);right=int(padding/2);left=padding-right; printf "%*s%s%*s|",left, "", $f[i],right,"" } ; print"" } }  END{printf "%s","+" ;for(j=0;j<fields_count;j++){for(i=0;i<15;i++) {printf "%s","-"};printf"+"}; print""}' "$table_name" `</tt>" 
-                                                	#set +x
-
-
 						fi
 					else
 						zenity --error --text="$table_name doesn't exist"
 					fi
-
-                                                #ClicktoClear
                                         ;;
                                         "6) Delee from Table") echo "$db_action"	#delete
-
-						
 						table_name=$(zenity --entry --title="Delete From Table" --text="Enter the table name :")
-						#read -p "Enter the  Table name : " table_name
 						condition=""
 						declare condition_arr
                                                 unset condition_arr
-                                                
-						
 						condition_arr=$(zenity --entry --title="Delete From Table" --text="Enter the condition : \nthe format shoulc be (e.g., salary > 5000) ")
 						IFS=" " read -ra condition_arr <<< "$condition_arr"
-						#echo "Enter the condition (e.g., salary >= 5000):"
-                                                #read -a condition_arr
                                                 condition_flag=false
-
 						unset all_fileds[@]
                                                 IFS=':' read -ra all_fields < $table_name
-
                                                 if [[ ${#condition_arr[@]} -ne 0  ]]
 						then
                     					for ((j=0;j<${#all_fields[@]};j++))
@@ -348,49 +300,32 @@ do
                                                         done
 							if [[ $condition_flag == false ]]
                                                         then
-                                                        	zenity --error --text="${condition_arr[0]} not found in table"
-                                                        	#echo "${condition_arr[0]} not found in table"
-                                                            
+                                                        	zenity --error --text="${condition_arr[0]} not found in table"                                                            
                                                         fi
-                                                                #echo "$condition  ${condition_arr[@]}"
                                                 	if [[ ${condition_arr[1]} == "=" ]]
 							then	
                                                         	condition_arr[1]="=="
 							fi
-
                                                 else
 							condition=0
                                                         echo "condition_arr is empty"
-                                                        #condition_flag=false
                                                 fi
 						condition_f="\$$condition ${condition_arr[1]} ${condition_arr[2]}"
-
 						awk -F: '{if (NR==1) print$0 ; if(NR!=1 && !('"$condition_f"')) print$0 }' $table_name  > temp && mv temp  $table_name 
-
                                                 ;;
                                         "7) Update Table") echo "$db_action"	#update
 						table_name=$(zenity --entry --title="Update Table" --text="Enter the table name :")
-						#read -p "Enter the  Table name : " table_name
 						unset desired_fields
 						unset value
 						set -f #this line disable "globing" which will allow the use to input the character "*" without converting it
-						#echo "Enter the field: (e.g (name1 name2 name3) )"
-						#read -a desired_fields
 						desired_fields=$(zenity --entry --title="Update Table" --text="enter the fields : \nthe format should be (name1 name2 name3)")
                                                 IFS=" " read -ra desired_fields <<< "$desired_fields"
 						value=$(zenity --entry --title="Update Table" --text="Enter the value to set :")
 						IFS=" " read -ra value <<< "$value"
-
-						#echo "Enter the values you want to set: (correspnding to the fields you chose) "
-						#read -a value
-						#declare -a pktmp
-                                                #declare -a  pk_arr
                                                 unset pk_arr
                                                 unset pk_tmp
                                                 pk_tmp=(`grep "^PK" constraints_files/$table_name`)
                                                 IFS=':' read -ra pk_arr <<< ${pk_tmp[@]}
-
-
 						declare -a last_fields
 						unset last_fields[@] #the column numbers which will be passed to the awk
 						unset all_fileds[@]
@@ -399,10 +334,7 @@ do
 						then
 							for((i=0;i<${#all_fields[@]};i++))
 							do
-								#if [[ ${pk_arr[2]} -ne $i  ]]
-								#then
 								last_fields=("${last_fields[@]}" "$(($i+1))")
-								#fi
 							done
 							IFS=':' read -ra desired_fields < $table_name #retrive allfields into desired fields
 							echo ${last_fields[@]}
@@ -415,39 +347,30 @@ do
                                                         	do
                                                                 	if [[ ${desired_fields[i]} == ${all_fields[j]} ]]
                                                                 	then
-                                                                        	#echo $j #debugging
                                                                         	last_fields=("${last_fields[@]}" "$(($j+1))")
-                                                                        	#echo ${last_fields[@]} #debugging
                                                                         	found=true
                                                                 	fi
                                                         	done
                                                                 	if [[ $found == false ]]
                                                                 	then
 										zenity --error --text="${desired_fields[i]} not found "
-                                                                        	#echo "${desired_fields[i]} not found "
                                                                         	break
                                                             		fi
                                                 	done
 						fi
-
 						declare -a const
 						unset const
                                                 IFS=':' read -ra const < constraints_files/$table_name 	#read attributes from file
 						right_datatype=true
-						echo ${last_fields[@]}
-						echo $const[@]
-						echo ${const[(($last_fields-1))]}
-
 						for ((i=0;i<${#last_fields[@]};i++ ))
 						do
 							if [[ ${const[((${last_fields[i]}-1))]} == "int" ]]
                                                 	then
                                                                 if [[ ${value[i]} =~ ^[0-9]+$ ]]
                                                                 then
-                                                                        echo "Number"
+                                                                        :
                                                                 else
                                                                         zenity --error --text="wrong data type of the column"
-									#echo "wrong data type of the column"
                                                                         right_datatype=false
                                                                 fi
                                                 	elif  [[ ${const[((${last_fields[i]}-1))]} == "char" ]]
@@ -455,20 +378,17 @@ do
                                                                 if [[ ${value[i]} =~ ^[0-9]*\.?[0-9]+$ ]]
                                                                 then
                                                                        zenity --error --text="wrong data type of the column"
-									#echo "wrong data type of the column"
-
                                                                         right_datatype=false
                                                                 else
-                                                                        echo "alpha"
+                                                                        :
                                                                 fi
                                                 	elif [[ ${const[((${last_fields[i]}-1))]} == "float" ]]
                                                 	then
                                                                 if [[ ${value[i]} =~ ^[0-9]*\.?[0-9]+$ ]]
                                                                 then
-                                                                        echo "float"
+                                                                        :
                                                                 else
 									zenity --error --text="wrong data type of the column"
-                                                                        #echo "wrong data type of the column"
                                                                         right_datatype=false
                                                                 fi
                                                 	fi
@@ -493,28 +413,18 @@ do
                                                          else
                                              	                 echo "not pk "
                                                          fi
-
 						done
-
-
 						set +f #re-enabling globing
-
-
 						if [[ $found == true && $right_datatype == true ]]
 						then
-
                                                 	condition=""
                                                 	declare condition_arr
                                                 	unset condition_arr
-
 							condition_arr=$(zenity --entry --title="Update Table" --text="Enter the condition : \nthe format shoulc be (e.g., salary > 5000) ")
                                                 	IFS=" " read -ra condition_arr <<< "$condition_arr"
-                                                	#echo "Enter the condition (e.g., salary >= 5000):"
-                                                	#read -a condition_arr
                                                 	condition_flag=false
                                                 	unset all_fileds[@]
                                                 	IFS=':' read -ra all_fields < $table_name
-
                                                 	if [[ ${#condition_arr[@]} -ne 0  ]]
                                                 	then
                                                         	for ((j=0;j<${#all_fields[@]};j++))
@@ -537,14 +447,10 @@ do
                                                         	fi
 							else
                                                         	echo "condition_arr is empty"
-                                                        	#condition_flag=false
                                                 	fi
                                                 	condition_f="\$$condition ${condition_arr[1]} ${condition_arr[2]}"
-
 							index=$(IFS=,; echo "${last_fields[*]}") #serialization into one string to pass it to the awk then we will split it inside of the awk
                                                 	desired_fields_s=$(IFS=,; echo "${desired_fields[*]}")
-                                                	#echo $desired_fields_s #for debugging
-
 							for((i=0;i<${#last_fields[@]};i++ ))
 							do
 							awk -F: '{if (NR==1) print$0 ; if(NR!=1 && '"$condition_f"') {OFS=FS; $'"${last_fields[$i]}"'="'"${value[$i]}"'" ;print$0 } else if(NR!=1) {print $0}}' $table_name  > temp && mv temp  $table_name
@@ -567,13 +473,11 @@ do
                         done
                 else
                         zenity --error --text="database not found"
-                        #ClicktoClear
                 fi
                         ;;
                 "4)drop database") echo "choosed $choice" #drops db
 			cd /home/$USER/bash-project/
 			db_del=$(zenity --entry --title="drop database" --text="Enter the name of the db you want to delete :")
-                        #read -p "enter the name of the db you want to delete : " db_del
                         if [[ -e $db_del ]]
                         then
                                 rm -rf /home/$USER/bash-project/$db_del
@@ -581,13 +485,9 @@ do
                         else
                                 zenity --error --title="drop database" --text="$db_del not found"
                         fi
-                        #ClicktoClear
                         ;;
                 "exit") flag=false
                         ;;
-                #*) echo "choose a number from 1 to 5"
-                #        ClicktoClear
-                #        ;;
         esac
 done
 ~                                                                                                                    
