dba71fd4fdb0e85c82548596eabaf076e2d08ae5
Moamen_Alghareeb <moamenalghareeb10@gmail.com>
2024-12-20 14:54:01 +0200

    insert last V

--- project.sh
+++ project.sh
@@ -215,8 +215,8 @@ do
 										echo "didin't enter the primary key ${pk_arr[1]}"
 										break
 									else
-										cut -d: -f${pk_arr[2]} $table_name | grep -q "$element"
-										if [[ $? -ne 0 ]]
+										cut -d: -f"$((${pk_arr[2]}+1))"  $table_name | grep -q "$element"
+										if [[ $? -eq 0 ]]
 										then
 											echo "id( primary key ) already exists"
 											break
